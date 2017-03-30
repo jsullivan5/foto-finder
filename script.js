e45ba451aef465874334bc36dfcc68bfc3f0a15d
James Sullivan <jamesfsullivan5@gmail.com>
2017-03-30 09:53:01 -0600

    add final refactor

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// Functions
 
+function ImageCard(cardTitle, cardImage, cardCaption) {
+  this.title = cardTitle;
+  this.image = cardImage
+  this.caption = cardCaption;
+}
+
+function filePath(filePath) {
+   return "photos\\" + filePath.split('\\').pop();
+}
+
+//Content Generation
 
 $('#save-image').on("click", function(){
   var cardTitle = $('#title').val();
   var cardCaption = $('#caption').val();
   var cardImage = filePath($('input:file').val());
-  console.log(cardImage);
   var newImageCard = new ImageCard(cardTitle, cardImage, cardCaption);
-  console.log(newImageCard);
   $("#gallery").prepend(`
     <div class="image-card">
       <h3 class="white">${cardTitle}</h3>
@@ -18,27 +28,44 @@ $('#save-image').on("click", function(){
       </section>
     </div>
     `
-  )
+  );
+  $('#add-file-text').text('');
 })
 
-// var cardTitle = $('#title').val();
-// var cardCaption = $('#caption').val();
-// var cardImage = filePath($('input:file').val());
-//
-// $(cardTitle).on('change', function () {
-//   console.log('change listener');
-// })
+$('#file').on('change', 'input:file', function() {
+  console.log('hearing file change');
+})
 
-function filePath(filePath) {
-   return "photos\\" + filePath.split('\\').pop();
-}
+// Disable Buttons
 
+$(document).on('input', 'input:text', 'input:file', function (e) {
+  var title = $('#title').val();
+  var caption = $('#caption').val();
+  var chooseFile = $('#file').val();
+  if (title != '' && caption != '' && chooseFile != '') {
+    $("#save-image").removeAttr("disabled");
+  } else {
+    $("#save-image").attr("disabled", "disabled");
+  }
+});
 
-function ImageCard(cardTitle, cardImage, cardCaption) {
-  this.title = cardTitle;
-  this.image = cardImage
-  this.caption = cardCaption;
-}
+$("document").ready(function(){
+  var title = $('#title').val();
+  var caption = $('#caption').val();
+  var chooseFile = $('#file').val();
+  $("#file").change(function() {
+    console.log($('um' + '#file').val());
+    console.log($('#caption').val());
+    console.log($('#title').val());
+    if (chooseFile != undefined) {
+      $("#save-image").removeAttr("disabled");
+    } else {
+      $("#save-image").attr("disabled", "disabled");
+    }
+  });
+});
+
+// Created Content Functionality
 
 $('#gallery').on('click','.click-trash', function() {
   console.log($('.click-trash').parent().parent());
@@ -49,4 +76,4 @@ $('#gallery').on('click','.click-heart', function() {
   console.log($('.click-heart').parent().parent());
   $(this).parent().parent().children().toggleClass('favorited');
   $(this).toggleClass('click-heart-like');
-});
+})
